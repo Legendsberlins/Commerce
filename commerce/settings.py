bf8b73eb9a73d5c83afed64e158ade0a87e25725
Legendsberlins <122905699+Legendsberlins@users.noreply.github.com>
2025-08-22 09:57:29 +0100

    White noise settings

--- commerce/settings.py
+++ commerce/settings.py
@@ -132,5 +132,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'auctions', 'static'),
 ]
+
+# Whitenoise settings for serving static files in production
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
