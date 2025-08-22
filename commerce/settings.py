bd7adb4e4ce328554a6db74eaa67cb104003d22b
Legendsberlins <122905699+Legendsberlins@users.noreply.github.com>
2025-08-22 09:50:31 +0100

    STATIC ROOT change

--- commerce/settings.py
+++ commerce/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '6ps8j!crjgrxt34cqbqn7x&b3y%(fny8k8nh21+qa)%ws3fh!q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['commerce-vjcr.onrender.com', 'localhost']
 
